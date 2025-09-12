70caf76a80896e5810a4d9990a11759ab08970e5
chandan904 <chandan143133sharma@gmail.com>
2025-09-12 18:48:30 +0000

    ques14

---
+++ 590022909-chandan-007-q14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'a' && ch <= 'z') {
+        
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ) {
+            printf("%c is a vowel.\n", ch);
+        } else {
+            printf("%c is a consonant.\n", ch);
+        }
+    } else 
+    
+
+    return 0;
+}
