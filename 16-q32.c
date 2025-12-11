88fa883f361baf187839390deaa93861d922029f
chandan904 <chandan143133sharma@gmail.com>
2025-12-11 09:35:47 +0000

    ques32

---
+++ 16-q32.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;  // store original number
+
+    // Reverse the number
+    while (num != 0) {
+        digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    // Check palindrome
+    if (original == reversed)
+        printf("%d is a palindrome.\n", original);
+    else
+        printf("%d is not a palindrome.\n", original);
+
+    return 0;
+}
